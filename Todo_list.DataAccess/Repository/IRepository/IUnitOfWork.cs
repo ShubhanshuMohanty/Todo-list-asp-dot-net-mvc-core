b4323ed31c9fbc2255e661b88bb7280620883d38
shubhanshu <shubhanshumohanty@gmail.com>
2025-07-05 16:54:56 +0530

    create unitofwork

---
+++ Todo_list.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Todo_list.DataAccess.Repository.IRepository
+{
+    public interface IUnitOfWork
+    {
+        void Save();
+    }
+}
---
+++ Todo_list.DataAccess/Repository/UnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Todo_list.DataAccess.Data;
+using Todo_list.DataAccess.Repository.IRepository;
+
+namespace Todo_list.DataAccess.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private ApplicationDbContext _db;
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
